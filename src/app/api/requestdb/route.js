83caa2b429ecbacc85e46cec8b01758cc7291132
ezshine <ezshine@live.cn>
2025-06-14 13:10:27 +0700

    feat:add new env vars USE_SHARED_DB

--- src/app/api/requestdb/route.js
+++ src/app/api/requestdb/route.js
@@ -13,11 +13,21 @@ const HiddenScreenNames = [
 ]
 
 export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const action = searchParams.get('action');
+  
+  if(Boolean(process.env.USE_SHARED_DB)){
+    const response = await fetch(`https://api.twitterxdownload.com/api/requestdb?${action?`action=${action}`:''}`);
+    const data = await response.json();
+    
+    return Response.json({
+      message: 'from shared database',
+      ...data
+    });
+  }
+
   try {
     await dbConnect();
-    
-    const { searchParams } = new URL(request.url);
-    const action = searchParams.get('action');
 
     const baseFilter = {
         screen_name: { $nin: HiddenScreenNames }
--- src/app/api/requestx/route.js
+++ src/app/api/requestx/route.js
@@ -2,17 +2,27 @@ import dbConnect from '@/lib/db';
 import Tweets from '@/lib/models/tweets';
 
 export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const tweet_id = searchParams.get('tweet_id');
+
+    if (!tweet_id) {
+        return Response.json({
+            success: false,
+            error: 'Tweet ID is required'
+        }, { status: 400 });
+    }
+
+    if(Boolean(process.env.USE_SHARED_DB)){
+        const response = await fetch(`https://api.twitterxdownload.com/api/requestx?${tweet_id?`tweet_id=${tweet_id}`:''}`);
+        const data = await response.json();
+        
+        return Response.json({
+          ...data,
+          message: 'from shared database'
+        });
+    }
+
     try {
-        const { searchParams } = new URL(request.url);
-        const tweet_id = searchParams.get('tweet_id');
-
-        if (!tweet_id) {
-            return Response.json({
-                success: false,
-                error: 'Tweet ID is required'
-            }, { status: 400 });
-        }
-
         await dbConnect();
         const tweet = await Tweets.findOne({ tweet_id: tweet_id });
         if (tweet) {
